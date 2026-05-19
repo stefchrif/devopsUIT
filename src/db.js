8041bf18c47affd432cfcfaba853800ff9e31297
Mustapha EL ATRASSI <mustapha.elat@gmail.com>
2026-05-19 11:45:00 +0000

    feat: create users table

--- src/db.js
+++ src/db.js
@@ -2,3 +2,5 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./data/database.db');
 function init() {}
 module.exports = { db, init };
+
+db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, role TEXT)');
