83a831936c5e16d44cda8897e83c2a7866ae6d5e
Mustapha EL ATRASSI <mustapha.elat@gmail.com>
2026-05-31 14:15:00 +0000

    feat: handle user session and auth logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,3 +2,5 @@ const express = require('express');
 const router = express.Router();
 router.get('/login', (req,res) => res.render('login'));
 module.exports = router;
+
+router.post('/login', (req,res) => res.redirect('/'));
