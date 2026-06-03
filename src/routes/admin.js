11ae2dee9d2280c5cde4766a6daf923bcd1c25b4
Mustapha EL ATRASSI <mustapha.elat@gmail.com>
2026-06-03 11:45:00 +0000

    feat: add product management page in backoffice

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,3 +2,5 @@ const express = require('express');
 const router = express.Router();
 router.get('/', (req,res) => res.render('admin/dashboard'));
 module.exports = router;
+
+router.get('/products', (req,res) => res.render('admin/products'));
