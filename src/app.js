256b2d30e903af3b617c5af5ec831403f6f18ecf
Mustapha EL ATRASSI <mustapha.elat@gmail.com>
2026-06-13 12:20:00 +0000

    fix: resolve EJS syntax error and configure layouts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layout');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
